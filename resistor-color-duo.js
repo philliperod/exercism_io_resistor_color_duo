24d47907971eb020f42dae3946c4f101a6c14032
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-09 10:57:29 -0500

    resubmiited by answer; refactor to remove type juggling with a more mathematical approach

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -2,8 +2,9 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+export const resistors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
 export const decodedValue = ([firstColor, secondColor]) => {
-  const resistors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
-  return parseInt(resistors.indexOf(firstColor).toString() + resistors.indexOf(secondColor).toString());
+  var numberColor = resistors.indexOf(firstColor) * 10 + resistors.indexOf(secondColor);
+  return numberColor;
 };
