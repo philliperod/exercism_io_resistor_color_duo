199154a339899b46da8370ccf6418d4741425cdc
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-13 08:28:20 -0500

    refactored into two functions for a more single responsibility principle application; submitted exercise

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -1,10 +1,7 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 export const resistors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
+export const resistorValue = (color) => resistors.indexOf(color);
+
 export const decodedValue = ([firstColor, secondColor]) => {
-  var numberColor = resistors.indexOf(firstColor) * 10 + resistors.indexOf(secondColor);
-  return numberColor;
+  return resistorValue(firstColor) * 10 + resistorValue(secondColor);
 };
